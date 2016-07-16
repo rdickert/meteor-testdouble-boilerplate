4eb932c7b091920d1308f0923d4270c9eeff0792
Anders Ramsay <andersr@gmail.com>
2016-07-16 12:45:19 -0400

    create Meteor app

--- features/create_note.feature
+++ features/create_note.feature
@@ -4,9 +4,10 @@ Feature: Create a new note
     I want to create a note,
     So that I can add note content
 
+  @focus
   Scenario: Create note
-    Given I am viewing a "New Note" input
-    And I can click on the "New Note" input
+    Given I am viewing a "New Note" field
+    And I can click on the "New Note" field
     When I type "My New Note" into the field
     And I hit the return key on my keyboard
     Then a note with the title "My New Note" should be created
---
+++ features/create_note_steps.js
@@ -0,0 +1,29 @@
+module.exports = function() {
+  
+  this.Given(/^I am viewing a "([^"]*)" field$/, function (arg1) {
+    // Write the automation code here
+    pending();
+  });
+
+  this.Given(/^I can click on the "([^"]*)" field$/, function (arg1) {
+    // Write the automation code here
+    pending();
+  });
+
+  this.When(/^I type "([^"]*)" into the field$/, function (arg1) {
+    // Write the automation code here
+    pending();
+  });
+
+  this.When(/^I hit the return key on my keyboard$/, function () {
+    // Write the automation code here
+    pending();
+  });
+
+  this.Then(/^a note with the title "([^"]*)" should be created$/, function (arg1) {
+    // Write the automation code here
+    pending();
+  });
+
+};
+
