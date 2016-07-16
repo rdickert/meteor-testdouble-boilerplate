d11ad81322e1ecb7cfda4ff2d3518bce6d07cd8d
Anders Ramsay <andersr@gmail.com>
2016-07-16 13:09:48 -0400

    Given I am viewing a 'New Note' field test

--- features/create_note/create_note_steps.js
+++ features/create_note/create_note_steps.js
@@ -1,29 +1,35 @@
 module.exports = function() {
   
   this.Given(/^I am viewing a "([^"]*)" field$/, function (arg1) {
-    // Write the automation code here
-    pending();
-  });
+
+  	const 
+  	  homepage       = 'http://localhost:3000',
+      new_note_field = 'input.note-title'
+
+    browser.url(homepage)
+    browser.waitForExist(new_note_field)
+
+  })
 
   this.Given(/^I can click on the "([^"]*)" field$/, function (arg1) {
     // Write the automation code here
-    pending();
-  });
+    pending()
+  })
 
   this.When(/^I type "([^"]*)" into the field$/, function (arg1) {
     // Write the automation code here
-    pending();
-  });
+    pending()
+  })
 
   this.When(/^I hit the return key on my keyboard$/, function () {
     // Write the automation code here
-    pending();
-  });
+    pending()
+  })
 
   this.Then(/^a note with the title "([^"]*)" should be created$/, function (arg1) {
     // Write the automation code here
-    pending();
-  });
+    pending()
+  })
 
-};
+}
 
